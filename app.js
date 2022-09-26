855c3c7fd4d794214c3d28ee4d4aab8a7f89cd7a
Ervis <lervis87@gmail.com>
2022-09-26 22:24:06 +0100

    window vs global

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ function sayHello(name) {
   console.log('Hello ' + name);
 }
 
-sayHello('Ervis')
+sayHello('Ervis');
+
+setTimeout(1) // delay calling an option
+clearTimeout()
+setInterval()
+
+// in browser you have window objects
+// so in browser they will all becmoe
+// window.objects
+// in node we have global instead of window.
+// 19 min
+// https://www.youtube.com/watch?v=TlB_eWDSMt4
